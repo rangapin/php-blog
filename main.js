aa372de10a9f33c20c6ddab75c174f665d8539c5
rangapin <rangapin@gmx>
2022-11-04 11:56:31 -0600

    front end completed

--- main.js
+++ main.js
@@ -18,3 +18,27 @@ const closeNav =() => {
 
 openNavBtn.addEventListener('click', openNav);
 closeNavBtn.addEventListener('click', closeNav);
+
+//sidebar
+
+const sidebar = document.querySelector('aside');
+const showSidebarBtn = document.querySelector('#show__sidebar-btn');
+const hideSidebarBtn = document.querySelector('hide__sidebar-btn');
+
+//show sidebar
+
+const showSidebar = () => {
+    sidebar.style.left = '0';
+    showSidebarBtn.style.display = 'none';
+    hideSidebarBtn.style.display = 'inline-block';
+}
+//hide sidebar
+
+const hideidebar = () => {
+    sidebar.style.left = '-100%';
+    showSidebarBtn.style.display = 'inline-block';
+    hideSidebarBtn.style.display = 'none';
+}
+
+showSidebarBtn.addEventListener('click', showSidebar);
+showSidebarBtn.addEventListener('click', showSidebar);
